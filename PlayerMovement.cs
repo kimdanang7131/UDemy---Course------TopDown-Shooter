5db0d3136de31e0bf19faecf95215d89758a321d
kimdanang7131 <ken7131@gmail.com>
2025-12-06 10:56:42 +0900

    25/12/06 - Section3 [Character Development] End

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,51 +1,111 @@
+using System;
 using UnityEngine;
-using VInspector.Libs;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
     private CharacterController characterController;
+    private Animator animator;
 
-    public float walkSpeed;
-    public Vector3 movementDirection;
+    [Header("Movement Info")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float speed;
+    private Vector3 movementDirection;
+    private float gravityScale = 9.81f;
+    private float verticalVelocity;
+    private bool isRunning;
+
+    [Header("Aim info")]
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
 
     private Vector2 moveInput;
     private Vector2 aimInput;
 
     void Awake()
     {
-        controls = new PlayerControls();
-
+        AssignInputEvents();
     }
+
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        speed = walkSpeed;
     }
 
     void Update()
     {
         ApplyMovement();
+        AimTowardsMouse();
+        AnimatorControllers();
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Fire");
+        animator.SetTrigger("Fire");
+    }
+
+    private void AnimatorControllers()
+    {
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+
+        animator.SetFloat("xVelocity", xVelocity, .1f, Time.deltaTime);
+        animator.SetFloat("zVelocity", zVelocity, .1f, Time.deltaTime);
+
+        bool playRunAnimation = isRunning && movementDirection.magnitude > 0;
+        animator.SetBool("isRunning", playRunAnimation);
+    }
+
+    private void AimTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0f;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
     }
 
     private void ApplyMovement()
     {
         movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        ApplyGravity();
 
-        if (movementDirection.magnitude > 0.1f)
+        if (movementDirection.magnitude > 0)
         {
-            characterController.Move(movementDirection * Time.deltaTime * walkSpeed);
+            characterController.Move(movementDirection * Time.deltaTime * speed);
         }
     }
 
-    private void Shoot()
+    private void ApplyGravity()
     {
-        Debug.Log("SHOOT");
+        if (characterController.isGrounded == false)
+        {
+            verticalVelocity -= gravityScale * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+            verticalVelocity = -.5f;
     }
 
-    void OnEnable()
+    #region New Input System
+
+    private void AssignInputEvents()
     {
-        controls.Enable();
+        controls = new PlayerControls();
 
         controls.Character.Fire.performed += context => Shoot();
 
@@ -53,11 +113,33 @@ public class PlayerMovement : MonoBehaviour
         controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
 
         controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
-        controls.Character.Aim.canceled += context => moveInput = Vector2.zero;
+        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+
+        controls.Character.Run.performed += context =>
+        {
+            if (movementDirection.magnitude > 0)
+            {
+                speed = runSpeed;
+                isRunning = true;
+            }
+        };
+
+        controls.Character.Run.canceled += context =>
+        {
+            speed = walkSpeed;
+            isRunning = false;
+        };
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
     }
 
     void OnDisable()
     {
         controls.Disable();
     }
+
+    #endregion
 }
